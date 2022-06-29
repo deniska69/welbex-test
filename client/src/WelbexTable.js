85b4d30a49b4e719e128f7e974264cc0a7f69cc3
Deniska <47465987+deniska69@users.noreply.github.com>
2022-06-30 05:07:52 +0700

    Разработана заготовка под страницу

---
+++ client/src/WelbexTable.js
@@ -0,0 +1,18 @@
+import "./WelbexTable.css";
+
+function WelbexTable() {
+  return (
+    <div className="welbex">
+      <div className="titleWelbex">
+        <h3>WelbeX - Тестовое задание</h3>
+      </div>
+      <div className="headerWelbex _shadow"></div>
+      <div className="tableWelbex _shadow">
+        <table></table>
+      </div>
+      <div className="footerWelbex _shadow"></div>
+    </div>
+  );
+}
+
+export default WelbexTable;
