e8d17b562b2d41d8858e71e7a073d04b22e9ffea
Deniska <47465987+deniska69@users.noreply.github.com>
2022-06-30 08:43:58 +0700

    Настроен вывод даты в таблицу в приемлемом формате

--- client/src/components/Table.js
+++ client/src/components/Table.js
@@ -1,9 +1,6 @@
 import './Table.css'
 
 const Table = ({ data }) => {
-  console.log(typeof data)
-  console.log(data)
-
   return (
     <div className="tableWelbex _shadow">
       <table>
@@ -18,7 +15,7 @@ const Table = ({ data }) => {
         <tbody>
           {data.map((row, index) => (
             <tr key={index}>
-              <td>{row.date}</td>
+              <td>{new Date(row.date).toLocaleString('ru-RU', { timeZone: 'Europe/Moscow' }).substr(0, 10)}</td>
               <td>{row.name}</td>
               <td>{row.count}</td>
               <td>{row.distance}</td>
